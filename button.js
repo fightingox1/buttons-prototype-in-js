95d42bcca1bcd2d10a34acc1ec90a36d417b0e5b
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-09-27 14:19:48 -0400

    Create button.js

---
+++ button.js
@@ -0,0 +1,17 @@
+
+    document.onkeydown = function(event) {
+        switch (event.keyCode) {
+           case 37:
+                alert('Left key pressed');
+              break;
+           case 38:
+                alert('Up key pressed');
+              break;
+           case 39:
+                alert('Right key pressed');
+              break;
+           case 40:
+                alert('Down key pressed');
+              break;
+        }
+    };
